a9cefb8d4b4cff319d9f95588de5a9f57139da3b
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-01-15 02:59:16 -0500

    Updated index.php

--- index.php
+++ index.php
@@ -8,12 +8,14 @@ require_once('vendor/autoload.php');
 
 // Create an instance of the Base class
 $f3 = Base::instance();
-$f3->set('DEBUG',3);
+$f3->set('DEBUG', 3);
 
-//Define a default route
-//$f3->route('GET/',function (){
-   // echo "<h1> Hello, world</h1>";
-//});
+
+//Define a default route (home page of our application)
+$f3->route('GET /', function (){
+  $view = new Template();
+  echo $view->render('views/home.html');
+});
 
 // Run fat free
 $f3->run();
