02a90c82c55f114cc23af7ef2b2337ee6024108a
nematu <nayaz-akhundzada@mail.greenriver.edu>
2021-01-15 02:46:14 -0500

    Added Index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+// Turn on error reporting
+ini_set('display_errors',TRUE);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class
+$f3 = Base::instance();
+$f3->set('DEBUG',3);
+
+//Define a default route
+//$f3->route('GET/',function (){
+   // echo "<h1> Hello, world</h1>";
+//});
+
+// Run fat free
+$f3->run();
